d3ae730a3d09af2c4e48bb35a4d476f7ec67fded
Daniel Freitas <daniel1208x@gmail.com>
2019-05-25 19:54:43 -0300

    Upgrade of Select2 version.

--- init.rb
+++ init.rb
@@ -11,9 +11,9 @@ end
 
 class ProjectSelect2ViewListener < Redmine::Hook::ViewListener
   def view_layouts_base_html_head(context)
-      javascript_include_tag('select2-3.4.5/select2.js', :plugin => :redmine_project_select2) +
+      javascript_include_tag('select2/dist/js/select2.min.js', :plugin => :redmine_project_select2) +
       javascript_include_tag('redmine_project_select2.js', :plugin => :redmine_project_select2) +
-      stylesheet_link_tag('select2-3.4.5/select2.css', :plugin => :redmine_project_select2)
+      stylesheet_link_tag('select2/dist/css/select2.min.css', :plugin => :redmine_project_select2)
   end
 end
 
